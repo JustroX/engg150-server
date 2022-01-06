54523ba48b6feaa721acf8a2cef8b57b132f74e7
JustroX <thepiesaresquared@gmail.com>
2022-01-06 21:06:16 +0800

    refactor: hash passwords

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from './user.schema';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) public model: Model<UserDocument>) {}
 
-  createUser(user: User) {
-    return this.model.create(user);
+  async createUser(user: User) {
+    const doc = await this.model.create(user);
+    const saltOrRounds = 10;
+    doc.password = await bcrypt.hash(user.password, saltOrRounds);
+    await doc.save();
+    return doc;
   }
 
   findOne(id: string) {
